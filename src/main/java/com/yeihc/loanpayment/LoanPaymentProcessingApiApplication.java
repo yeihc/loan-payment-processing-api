b8dccbeda3f4f6f787d22a00a11cc91f59c733e2
ASUSGAMA16\yeihc <yeisson72@outlook.com>
2026-02-08 18:27:18 -0600

    Create initial project base and clean

---
+++ src/main/java/com/yeihc/loanpayment/LoanPaymentProcessingApiApplication.java
@@ -0,0 +1,13 @@
+package com.yeihc.loanpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanPaymentProcessingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanPaymentProcessingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yeihc/loanpayment/LoanPaymentProcessingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeihc.loanpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanPaymentProcessingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
