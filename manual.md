39c7fe65203fb4c94e47dcf77643cfc0c2ec463b
ASUSGAMA16\yeihc <yeisson72@outlook.com>
2026-02-11 20:05:22 -0600

    feat: bootstrap project structure, config, and health endpoint

---
+++ manual.md
@@ -0,0 +1,10 @@
+Consola H2
+
+Acceso a http://localhost:8080/h2-console.
+
+Puedes usar estas credenciales:
+
+Driver: org.h2.Driver
+JDBC URL: jdbc:h2:mem:testdb
+Username: sa
+Password: (vacío)
--- src/main/java/com/yeihc/loanpayment/LoanPaymentProcessingApiApplication.java
+++ src/main/java/com/yeihc/loanpayment/LoanPaymentProcessingApiApplication.java
@@ -3,7 +3,7 @@ package com.yeihc.loanpayment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.yeihc")
 public class LoanPaymentProcessingApiApplication {
 
 	public static void main(String[] args) {
